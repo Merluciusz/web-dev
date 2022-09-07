fb2ffeb72e79d96902c960d713eba29f7461927c
Merluciusz <limestoragesolutions@gmail.com>
2022-09-07 17:43:56 +0300

    Async/Await

--- Stage12/index.js
+++ Stage12/index.js
@@ -1,25 +1,33 @@
-console.log('index script');
+// async function myFunc(){
+//   console.log('in the function');
+//   return 'hello';
+// }
 
-const http = new EasyHTTP();
+// myFunc().then(res => console.log(res));
 
-// http.get('https://jsonplaceholder.typicode.com/todos')
-// .then(data => console.log(data))
-// .catch(err => console.log(err));
+async function execRequest(){
+  
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => resolve('Connecting .. Connected'), 2000);
+  })
+  const error = false;
 
-const data = {
-  name:'john Slow',
-  username: 'johnslow',
-  email: 'johnslow@qmail.com'
+  if(!error){
+    const res = await promise;
+    return res;
+  }else{
+    await Promise.reject(new Error('something went wrong'));
+  }
 }
 
-// http.post('https://jsonplaceholder.typicode.com/users', data)
-// .then(data => console.log(data))
-// .catch(err => console.log(err));
+//execRequest().then(res => console.log(res)).catch(err => console.log(err));
 
-// http.put('https://jsonplaceholder.typicode.com/users/2', data)
-// .then(data => console.log(data))
-// .catch(err => console.log(err));
+async function getUsers(){
+  const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  // only proceed once its resolved
+  const data = await response.json();
+  // only proceed once second promise is resolved
+  return data;
+ }
 
-http.delete('https://jsonplaceholder.typicode.com/users/2')
-.then(data => console.log(data))
-.catch(err => console.log(err));
+ getUsers().then(users => console.log(users));
